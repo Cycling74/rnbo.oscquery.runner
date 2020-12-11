37235f11ddfb7ac3e1f3f1b5a2aaf833c5ac70bd
Alex Norman <xnor@cycling74.com>
2020-12-11 14:25:19 -0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 #A quick/dirty script to deploy to a rpi named 'rpi'.. this should be replaced by apt or something
 HOST=rpi
-rsync -avz --exclude build/ --delete ../RNBOJackRunner ${HOST}:local/src/
+rsync -avz --exclude build/ --delete ../RNBOOSCQueryRunner ${HOST}:local/src/
 rsync -avz --exclude build/ --delete ../../src/cpp/ ${HOST}:local/src/rnbo
