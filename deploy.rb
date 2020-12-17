0c8a671c19801778237901284a74d442c7cf5318
Alex Norman <xnor@cycling74.com>
2020-12-17 11:37:57 -0800

    better deploy script

---
+++ deploy.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#A quick/dirty script to deploy to a rpi.. this should be replaced by apt or something
+remote = ARGV[0]
+raise "you must provide a remote, eg pi@c74rpi.local name as the first argument" unless remote
+system("rsync -avz --exclude build/ --exclude deploy.rb --delete ../RNBOOSCQueryRunner #{remote}:local/src/")
+system("rsync -avz --exclude build/ --delete ../../src/cpp/ #{remote}:local/src/rnbo")
