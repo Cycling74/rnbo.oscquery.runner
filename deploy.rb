abc6e5d673f22b6757f27a3268c44c40f38d8bfc
Alex Norman <xnor@cycling74.com>
2021-01-25 13:04:25 -0800

    don't send docker dir

--- deploy.rb
+++ deploy.rb
@@ -2,5 +2,5 @@
 #A quick/dirty script to deploy to a rpi.. this should be replaced by apt or something
 remote = ARGV[0]
 raise "you must provide a remote, eg pi@c74rpi.local name as the first argument" unless remote
-system("rsync -avz --exclude build/ --exclude deploy.rb --delete ../RNBOOSCQueryRunner #{remote}:local/src/")
+system("rsync -avz --exclude build/ --exclude RNBOOSCQueryRunner/docker/ --exclude deploy.rb --delete ../RNBOOSCQueryRunner #{remote}:local/src/")
 system("rsync -avz --exclude build/ --delete ../../src/cpp/ #{remote}:local/src/rnbo")
