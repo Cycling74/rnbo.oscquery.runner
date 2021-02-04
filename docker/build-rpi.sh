e788f392bf5b8242b8d826fad8f45be5671660fe
Alex Norman <xnor@cycling74.com>
2021-02-04 12:12:50 -0800

    release build

--- docker/build-rpi.sh
+++ docker/build-rpi.sh
@@ -4,6 +4,7 @@ mkdir build-rpi && cd build-rpi
 cp ../config/conan-rpi-xcompile ~/.conan/profiles/rpi && \
 	PATH=/opt/cross-pi-gcc/bin:$PATH \
 	cmake -DRNBO_DIR=/build/src/cpp/ \
+	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_TOOLCHAIN_FILE=../config/rpi-xpile-toolchain.cmake \
 	-DCONAN_PROFILE=rpi \
 	..  && make && cpack
