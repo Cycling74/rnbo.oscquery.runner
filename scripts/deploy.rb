34c7da9801598b7927244f86b003539d1ecac4de
Alex Norman <xnor@cycling74.com>
2021-02-09 10:54:04 -0800

    ditch .ccls-cache in deploy

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -2,5 +2,5 @@
 #A quick/dirty script to deploy to an rpi for dev work
 remote = ARGV[0]
 raise "you must provide a remote, eg pi@c74rpi.local name as the first argument" unless remote
-system("rsync -avz --exclude 'build*/' --exclude RNBOOSCQueryRunner/docker/ --exclude deploy.rb --delete ../RNBOOSCQueryRunner #{remote}:local/src/")
+system("rsync -avz --exclude 'build*/' --exclude RNBOOSCQueryRunner/docker/ --exclude deploy.rb --exclude '.ccls-cache/' --delete ../RNBOOSCQueryRunner #{remote}:local/src/")
 system("rsync -avz --exclude 'build*/' --delete ../../src/cpp/ #{remote}:local/src/rnbo")
