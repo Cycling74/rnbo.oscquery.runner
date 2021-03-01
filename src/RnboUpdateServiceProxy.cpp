634566d2892e9c4d446b3de19f55bf8ee163fbc7
Alex Norman <xnor@cycling74.com>
2021-03-01 15:16:24 -0800

    add property callbacks

--- src/RnboUpdateServiceProxy.cpp
+++ src/RnboUpdateServiceProxy.cpp
@@ -12,13 +12,31 @@ RnboUpdateServiceProxy::RnboUpdateServiceProxy(std::string destination, std::str
 
 RnboUpdateServiceProxy::~RnboUpdateServiceProxy() { unregisterProxy(); }
 
+void RnboUpdateServiceProxy::setActiveCallback(std::function<void(bool)> cb) {
+	std::lock_guard<std::mutex> guard(mCallbackMutex);
+	mActiveCallback = cb;
+}
+void RnboUpdateServiceProxy::setStatusCallback(std::function<void(std::string)> cb) {
+	std::lock_guard<std::mutex> guard(mCallbackMutex);
+	mStatusCallback = cb;
+}
+
 void RnboUpdateServiceProxy::onPropertiesChanged(
 		const std::string& interfaceName,
 		const std::map<std::string, sdbus::Variant>& changedProperties,
 		const std::vector<std::string>& invalidatedProperties) {
-	cout << "got prop change " << endl;
+	std::lock_guard<std::mutex> guard(mCallbackMutex);
 	for (auto& kv: changedProperties) {
-		cout << "key " << kv.first << endl;
+		auto& key = kv.first;
+		auto& var = kv.second;
+		if (key == "Status") {
+			if (mStatusCallback && var.containsValueOfType<std::string>()) {
+				mStatusCallback(var.get<std::string>());
+			}
+		} else if (key == "Active") {
+			if (mActiveCallback && var.containsValueOfType<bool>()) {
+				mActiveCallback(var.get<bool>());
+			}
+		}
 	}
-	//TODO
 }
--- src/RnboUpdateServiceProxy.h
+++ src/RnboUpdateServiceProxy.h
@@ -2,6 +2,9 @@
 
 #include <map>
 #include <vector>
+#include <functional>
+#include <mutex>
+
 #include <sdbus-c++/sdbus-c++.h>
 #include "UpdateServiceProxyGlue.h"
 
@@ -10,7 +13,14 @@ class RnboUpdateServiceProxy : public sdbus::ProxyInterfaces<com::cycling74::rnb
 	public:
 		RnboUpdateServiceProxy(std::string destination = std::string(com::cycling74::rnbo_proxy::INTERFACE_NAME), std::string objectPath = "/com/cycling74/rnbo");
 		virtual ~RnboUpdateServiceProxy();
+
+		void setActiveCallback(std::function<void(bool)> cb);
+		void setStatusCallback(std::function<void(std::string)> cb);
 	protected:
+		std::function<void(bool)> mActiveCallback;
+		std::function<void(std::string)> mStatusCallback;
+		std::mutex mCallbackMutex;
+
 		virtual void onPropertiesChanged(
 				const std::string& interfaceName,
 				const std::map<std::string, sdbus::Variant>& changedProperties,
