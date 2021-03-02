81d49b5a44970e7dca6184a5cf32ce9f38600ab0
Alex Norman <xnor@cycling74.com>
2021-03-01 17:05:50 -0800

    update version, can cross compile from docker image!

--- docker/build-update-rpi.sh
+++ docker/build-update-rpi.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 cd /build/examples/RNBOOSCQueryRunner/update/
-rm -rf build-rpi
 mkdir build-rpi && cd build-rpi
-PATH=/opt/cross-pi-gcc/bin:$PATH \
+cp ../../config/conan-rpi-xcompile ~/.conan/profiles/rpi && \
+	PATH=/opt/cross-pi-gcc/bin:$PATH \
 	cmake \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DSDBUS_DIR=/sdbus-0.8.3 \
 	-DCMAKE_TOOLCHAIN_FILE=../../config/rpi-xpile-toolchain.cmake \
 	..  && make && cpack
