f49820a6c9dc3ccd2bda66d5271d3f9170028787
Alex Norman <xnor@cycling74.com>
2021-04-28 19:40:54 -0700

    can build with main c++ headers
    and a bunch of other stuff in the includes, haven't tried linking yet though

--- 3rdparty/ossia/conanfile.py
+++ 3rdparty/ossia/conanfile.py
@@ -28,17 +28,37 @@ class LibossiaConan(ConanFile):
     def configure_cmake(self):
         cmake = CMake(self)
 
-        cmake.definitions["OSSIA_CPP"] = True
-        cmake.definitions["OSSIA_CPP_ONLY"] = True
         cmake.definitions["OSSIA_DATAFLOW"] = False
         cmake.definitions["OSSIA_EDITOR"] = False
         cmake.definitions["OSSIA_GFX"] = False
-        cmake.definitions["OSSIA_PCH"] = False
-        cmake.definitions["OSSIA_PROTOCOL_ARTNET"] = False
+        cmake.definitions["OSSIA_JAVA"] = False
+        cmake.definitions["OSSIA_PD"] = False
+        cmake.definitions["OSSIA_MAX"] = False
+        cmake.definitions["OSSIA_PYTHON"] = False
+        cmake.definitions["OSSIA_QT"] = False
+        cmake.definitions["OSSIA_C"] = False
+        cmake.definitions["OSSIA_CPP"] = True
+        cmake.definitions["OSSIA_UNITY3D"] = False
+        cmake.definitions["OSSIA_QML"] = False
+        cmake.definitions["OSSIA_QML_SCORE"] = False
+        cmake.definitions["OSSIA_QML_DEVICE"] = False
+        cmake.definitions["OSSIA_DISABLE_QT_PLUGIN"] = True
+        cmake.definitions["OSSIA_DNSSD"] = True
+
         cmake.definitions["OSSIA_PROTOCOL_AUDIO"] = False
         cmake.definitions["OSSIA_PROTOCOL_MIDI"] = False
-        cmake.definitions["OSSIA_PROTOCOL_WIIMOTE"] = False
+        cmake.definitions["OSSIA_PROTOCOL_OSC"] = True
+        cmake.definitions["OSSIA_PROTOCOL_MINUIT"] = False
+        cmake.definitions["OSSIA_PROTOCOL_OSCQUERY"] = True
+        cmake.definitions["OSSIA_PROTOCOL_HTTP"] = False
+        cmake.definitions["OSSIA_PROTOCOL_WEBSOCKETS"] = False
+        cmake.definitions["OSSIA_PROTOCOL_SERIAL"] = False
+        cmake.definitions["OSSIA_PROTOCOL_PHIDGETS"] = False
+        cmake.definitions["OSSIA_PROTOCOL_LEAPMOTION"] = False
         cmake.definitions["OSSIA_PROTOCOL_JOYSTICK"] = False
+        cmake.definitions["OSSIA_PROTOCOL_WIIMOTE"] = False
+        cmake.definitions["OSSIA_PROTOCOL_ARTNET"] = False
+        cmake.definitions["OSSIA_PROTOCOL_LIBMAPPER"] = False
 
         return cmake
 
@@ -52,6 +72,5 @@ class LibossiaConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        print("CALLING package_info")
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs = ["include"]
