abdc49d968225d0c9e62428e6efa04519ec79a6a
Alex Norman <xnor@cycling74.com>
2021-05-07 15:25:35 -0700

    copy the conan config, make it default

--- docker/build-rpi.sh
+++ docker/build-rpi.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd /build/examples/RNBOOSCQueryRunner/
 mkdir build-rpi && cd build-rpi
+mkdir -p ~/.conan/profiles/
+cp ../config/conan-rpi-xcompile ~/.conan/profiles/default
 PATH=/opt/cross-pi-gcc/bin:$PATH \
 	cmake -DRNBO_DIR=/build/src/cpp/ \
 	-DCMAKE_BUILD_TYPE=Release \
