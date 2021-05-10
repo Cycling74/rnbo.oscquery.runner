c729eee6a72335affb135873b743fc3d39cda63d
Alex Norman <xnor@cycling74.com>
2021-05-10 12:10:43 -0700

    use conan for sdbus

--- docker/build-update-rpi.sh
+++ docker/build-update-rpi.sh
@@ -5,6 +5,5 @@ cp ../../config/conan-rpi-xcompile ~/.conan/profiles/default && \
 	PATH=/opt/cross-pi-gcc/bin:$PATH \
 	cmake \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DSDBUS_DIR=/sdbus-0.8.3 \
 	-DCMAKE_TOOLCHAIN_FILE=../../config/rpi-xpile-toolchain.cmake \
 	..  && make && cpack
