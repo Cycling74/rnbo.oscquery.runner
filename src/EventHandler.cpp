25efec4035c532368c733747ea35e354130b9f02
Stefan Brunner <stb@cycling74.com>
2022-02-11 12:35:24 +0100

    make queue events const references
    see #16386

--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -11,23 +11,23 @@ void EventHandler::eventsAvailable() {
 	//TODO
 }
 
-void EventHandler::handlePresetEvent(RNBO::PresetEvent event) {
+void EventHandler::handlePresetEvent(const RNBO::PresetEvent& event) {
 	//TODO
 }
 
-void EventHandler::handleParameterEvent(RNBO::ParameterEvent event) {
+void EventHandler::handleParameterEvent(const RNBO::ParameterEvent& event) {
 	if (mParameterCallback) {
 		mParameterCallback(event.getIndex(), event.getValue());
 	}
 }
 
-void EventHandler::handleMessageEvent(RNBO::MessageEvent event) {
+void EventHandler::handleMessageEvent(const RNBO::MessageEvent& event) {
 	if (mMessageCallback) {
 		mMessageCallback(event);
 	}
 }
 
-void EventHandler::handleMidiEvent(RNBO::MidiEvent event) {
+void EventHandler::handleMidiEvent(const RNBO::MidiEvent& event) {
 	if (mMidiCallback) {
 		mMidiCallback(event);
 	}
