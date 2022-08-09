c4016b4630eff513f2492e2eb10db377e0efbd86
Alex Norman <xnor@cycling74.com>
2022-08-08 17:08:58 -0700

    building for bull 32bit
    not 64 yet

--- common/sdbus.cmake
+++ common/sdbus.cmake
@@ -6,23 +6,23 @@ if (SDBUS_CODEGEN)
 endif()
 
 #static linking to LGPL is okay since we provide the source code to this project, users could build and relink
-conan_cmake_configure(
-	REQUIRES sdbus-cpp/0.8.3
-	GENERATORS cmake_find_package
-	OPTIONS
-		sdbus-cpp:shared=False
-		sdbus-cpp:with_code_gen=${CODEGEN}
-	IMPORTS "bin, *.dll -> ./lib"
-	IMPORTS "bin, sdbus-c++-xml2cpp -> ./"
-	IMPORTS "lib, *.dylib* -> ./lib"
-)
-conan_cmake_autodetect(settings)
-conan_cmake_install(
-	PATH_OR_REFERENCE .
-	BUILD missing
-	#REMOTE cycling-public
-	SETTINGS ${settings}
-)
-find_package(sdbus-c++ REQUIRED)
+#conan_cmake_configure(
+#	REQUIRES sdbus-cpp/0.8.3
+#	GENERATORS cmake_find_package
+#	OPTIONS
+#		sdbus-cpp:shared=False
+#		sdbus-cpp:with_code_gen=${CODEGEN}
+#	IMPORTS "bin, *.dll -> ./lib"
+#	IMPORTS "bin, sdbus-c++-xml2cpp -> ./"
+#	IMPORTS "lib, *.dylib* -> ./lib"
+#)
+#conan_cmake_autodetect(settings)
+#conan_cmake_install(
+#	PATH_OR_REFERENCE .
+#	BUILD missing
+#	#REMOTE cycling-public
+#	SETTINGS ${settings}
+#)
+find_package(sdbus-c++ 0.8.3 REQUIRED)
 set(SDBUS_LIBS ${SDBusCpp_LIBRARIES})
 include_directories(${SDBusCpp_INCLUDE_DIRS})
