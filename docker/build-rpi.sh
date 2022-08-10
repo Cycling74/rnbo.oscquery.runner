c52a19bd6a8a476e7fc9fb9e8ab8f27a9cb0057f
Alex Norman <xnor@cycling74.com>
2022-08-10 14:40:01 -0700

    force rm

--- docker/build-rpi.sh
+++ docker/build-rpi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /build/examples/RNBOOSCQueryRunner/
-mkdir -p build-rpi && cd build-rpi && rm -r *
+mkdir -p build-rpi && cd build-rpi && rm -fr *
 mkdir -p ~/.conan/profiles/ && cp ../config/conan-rpi-xcompile ~/.conan/profiles/default
 PATH=/opt/cross-pi-gcc/bin:$PATH \
 	cmake -DRNBO_DIR=/build/src/cpp/ \
