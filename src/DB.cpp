ecacfed4d5f721d6a1fe2d8e225692719dae8e55
Alex Norman <xnor@cycling74.com>
2023-05-15 13:34:55 -0700

    simplify migration and add indexes

--- src/DB.cpp
+++ src/DB.cpp
@@ -10,15 +10,6 @@ namespace fs = boost::filesystem;
 
 namespace {
 	const std::string rnbo_version(RNBO_VERSION);
-	void do_migration(int curversion, SQLite::Database& db, int version, std::function<void(SQLite::Database&)> func) {
-		if (curversion < version) {
-			func(db);
-			SQLite::Statement query(db, "INSERT INTO migrations (id, rnbo_version) VALUES (?, ?)");
-			query.bind(1, version);
-			query.bind(2, rnbo_version);
-			query.exec();
-		}
-	}
 }
 
 DB::DB() : mDB(config::get<fs::path>(config::key::DBPath).get().string(), SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE) {
@@ -38,17 +29,27 @@ DB::DB() : mDB(config::get<fs::path>(config::key::DBPath).get().string(), SQLite
 		query.exec();
 	}
 
-	int cur = 0;
+	int curversion = 0;
 	{
 		SQLite::Statement query(mDB, "SELECT MAX(id) FROM migrations");
 		if (!query.executeStep()) {
 			throw new std::runtime_error("failed to get result from migrations");
 		}
 		//migrate
-		cur = query.getColumn(0);
+		curversion = query.getColumn(0);
 	}
 
-	do_migration(cur, mDB, 2, [](SQLite::Database& db) {
+	auto do_migration = [curversion, this](int version, std::function<void(SQLite::Database&)> func) {
+		if (curversion < version) {
+			func(mDB);
+			SQLite::Statement query(mDB, "INSERT INTO migrations (id, rnbo_version) VALUES (?, ?)");
+			query.bind(1, version);
+			query.bind(2, rnbo_version);
+			query.exec();
+		}
+	};
+
+	do_migration(2, [](SQLite::Database& db) {
 			db.exec(R"(CREATE TABLE IF NOT EXISTS patchers (
 					id INTEGER PRIMARY KEY AUTOINCREMENT,
 					name TEXT NOT NULL,
@@ -60,6 +61,10 @@ DB::DB() : mDB(config::get<fs::path>(config::key::DBPath).get().string(), SQLite
 			)
 			)"
 			);
+	});
+	do_migration(3, [](SQLite::Database& db) {
+			db.exec("CREATE INDEX patcher_version ON patchers(runner_rnbo_version)");
+			db.exec("CREATE INDEX patcher_name_version ON patchers(name, runner_rnbo_version)");
 	});
 }
 
