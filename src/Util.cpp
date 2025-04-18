b7f205265d0165a9afeeecaf3ffc3260459bde41
Alex Norman <xnor@cycling74.com>
2025-04-18 08:37:33 -0700

    new utility setup, render time template

---
+++ src/Util.cpp
@@ -0,0 +1,20 @@
+#include "Util.h"
+
+#include <iostream>
+#include <chrono>
+#include <iterator>
+
+namespace runner {
+	std::string render_time_template(const std::string& tmpl) {
+		const size_t size = 2 * (tmpl.size() + 1); //just pad it out a bunch
+		std::vector<char> filename(size);
+
+		std::time_t time = std::time({});
+		if (std::strftime(filename.data(), size, tmpl.c_str(), std::gmtime(&time)) == 0) {
+			std::cerr << "failed to render to time template " << tmpl << std::endl;
+			filename[size - 1] = '\0';
+		}
+
+		return std::string(filename.data());
+	}
+}
---
+++ src/Util.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace runner {
+	std::string render_time_template(const std::string& templ);
+}
