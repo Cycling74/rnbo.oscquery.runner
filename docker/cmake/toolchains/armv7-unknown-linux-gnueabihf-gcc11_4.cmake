0b01931b1b758b3c9bee073821a74c5a22e38218
Alex Norman <xnor@cycling74.com>
2026-04-01 16:54:57 -0700

    update CMAKE_FIND_ROOT_PATH to find sdbus

--- docker/cmake/toolchains/armv7-unknown-linux-gnueabihf-gcc11_4.cmake
+++ docker/cmake/toolchains/armv7-unknown-linux-gnueabihf-gcc11_4.cmake
@@ -8,7 +8,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_SYSROOT ${SYSROOT})
-set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf/)
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf/ /usr/lib/arm-linux-gnueabihf)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
